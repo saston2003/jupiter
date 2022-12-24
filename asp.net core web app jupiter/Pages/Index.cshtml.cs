1563122e921cc38ac969685a41a89e9bb91411f0
saston2003@outlook.com <saston2003@outlook.com>
2022-12-24 18:21:37 +0000

    initial commit with welcome steve

---
+++ asp.net core web app jupiter/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace asp.net_core_web_app_jupiter.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
